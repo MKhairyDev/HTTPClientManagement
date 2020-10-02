644aa85052c816bacfb5aabf9f30e7bcb66c7ca5
Moemen Saad <moemen.saad@nuance.com>
2020-10-02 18:03:39 +0200

    Adding Azure Cache for redis

--- RealState.ReadStack/AgentStatisticsQueries.cs
+++ RealState.ReadStack/AgentStatisticsQueries.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using APIConsumer;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using RealState.Models;
 
 namespace RealState.ReadStack
@@ -15,22 +17,54 @@ namespace RealState.ReadStack
 
         private readonly RealStateStatisticsClient _statisticsClient;
         private readonly int topNumberNeeded = 10;
-
-        public AgentStatisticsQueries(RealStateStatisticsClient statisticsClient)
+        IDistributedCache _distributedCache;
+        public AgentStatisticsQueries(RealStateStatisticsClient statisticsClient,IDistributedCache distributedCache)
         {
             _statisticsClient = statisticsClient ?? throw new ArgumentNullException(nameof(statisticsClient));
+            _distributedCache = distributedCache;
         }
 
         public async Task<IEnumerable<MakelaarObject>> GetTopAgentsByObjects(bool withGarden)
         {
-            var objectList = await _statisticsClient.GetRealStateObjects(withGarden, _cancellationTokenSource.Token);
+            List<MakelaarObject> rsObjects =null;
+            string cachStr = null;
 
-            return objectList.Objects.GroupBy(x => x.MakelaarId)
-                .OrderByDescending(g => g.Count()).Take(topNumberNeeded)
-                .ToList()
-                .Select(agentGroup => new MakelaarObject
-                    {MakelaarId = agentGroup.Key, ObjectsNumber = agentGroup.Count()})
-                .ToList();
+            //Check if the value in the cache first
+            if (withGarden)
+            {
+                cachStr = _distributedCache.GetString("rs-WithGarden");
+            }
+            else
+            {
+                cachStr = _distributedCache.GetString("rs-NoGarden");
+
+            }
+            if (!string.IsNullOrEmpty(cachStr))
+            {
+                rsObjects = JsonConvert.DeserializeObject<List<MakelaarObject>>(cachStr);
+            }
+            else
+            {
+                var allObj = await _statisticsClient.GetRealStateObjects(withGarden, _cancellationTokenSource.Token);
+                rsObjects = allObj.Objects.GroupBy(x => x.MakelaarId)
+             .OrderByDescending(g => g.Count()).Take(topNumberNeeded).ToList().Select(agentGroup => new MakelaarObject { MakelaarId = agentGroup.Key, ObjectsNumber = agentGroup.Count() }).ToList();
+
+                //Setting cache invalidation policy
+                DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+                cacheEntryOptions.SetAbsoluteExpiration(new TimeSpan(12, 0, 0));
+                if (withGarden)
+                {
+                    _distributedCache.SetString("rs-WithGarden", JsonConvert.SerializeObject(rsObjects), cacheEntryOptions);
+                }
+                else
+                {
+                    _distributedCache.SetString("rs-NoGarden", JsonConvert.SerializeObject(rsObjects), cacheEntryOptions);
+                }
+
+            }
+
+            return rsObjects;
+            
         }
     }
 }
--- RealState.Statistics.Web/Startup.cs
+++ RealState.Statistics.Web/Startup.cs
@@ -2,6 +2,7 @@
 using APIConsumer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RealState.ReadStack;
 
@@ -9,6 +10,11 @@ namespace RealState.Statistics.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -53,6 +59,10 @@ namespace RealState.Statistics.Web
              */
             services.AddSingleton<IPolicyService,PolicyService>();
             services.AddApplicationInsightsTelemetry();
+          
+            services.AddDistributedRedisCache(options =>
+                options.Configuration=Configuration.GetConnectionString("RedisConnection")); ;
+            
         }
 
 
